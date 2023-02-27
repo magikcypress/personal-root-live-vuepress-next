3f59b97863dfb3ee2ba08dca10aa2f3dd69be151
magikcypress <cyp@rouquin.me>
2023-02-27 17:11:12 +0100

    Add component LiveArchive

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -1,11 +1,13 @@
 import { defineClientConfig } from '@vuepress/client'
 import MyLive from './components/MyLive.vue'
 import MyAudio from './components/MyAudio.vue'
+import LiveArchive from './components/LiveArchive.vue'
 
 export default defineClientConfig({
   enhance({ app, router, siteData }) {
     app.component('MyLive', MyLive),
-    app.component('MyAudio', MyAudio)
+    app.component('MyAudio', MyAudio),
+    app.component('LiveArchive', LiveArchive)
   },
   setup() {},
   rootComponents: [],
---
+++ docs/.vuepress/components/LiveArchive.vue
@@ -0,0 +1,14 @@
+<template>
+    <video id="video" autoplay="true" controls="controls" poster="/img/cat.jpg" :src="source"></video>
+</template>
+<script>
+export default {
+  name: 'LiveArchive',
+  props: {
+    source: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
